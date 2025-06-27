860913095030abe3cbb47536148e09293ca79ac6
ASerhii <153670129+ASerhii@users.noreply.github.com>
2025-06-27 18:50:14 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/ibiza', (req, res) => {
+  const url = 'http://95.163.208.18:8000/;stream/1';
+  request
+    .get(url)
+    .on('error', (err) => {
+      console.error('Stream error:', err);
+      res.sendStatus(500);
+    })
+    .pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
