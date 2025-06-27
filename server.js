ce7598ac05ea1668935ca1327f5945b502f5fb34
ASerhii <153670129+ASerhii@users.noreply.github.com>
2025-06-27 19:43:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const express = require('express');
-const request = require('request');
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/ibiza', (req, res) => {
-  const url = 'http://air.radiorecord.ru:805/ibiza_320';
-  request
-    .get(url)
-    .on('error', (err) => {
-      console.error('Stream error:', err);
-      res.sendStatus(500);
-    })
-    .on('response', (response) => {
-      res.setHeader('Content-Type', 'audio/mpeg');
-    })
-    .pipe(res);
+  const options = {
+    hostname: 'air.radiorecord.ru',
+    port: 805,
+    path: '/ibiza_320',
+    method: 'GET',
+    headers: {
+      'Icy-MetaData': '1',
+      'User-Agent': 'Mozilla/5.0' // деякі стріми вимагають юзер-агента
+    }
+  };
+
+  const streamReq = http.request(options, streamRes => {
+    res.setHeader('Content-Type', 'audio/mpeg');
+    streamRes.pipe(res);
+  });
+
+  streamReq.on('error', err => {
+    console.error('Stream error:', err);
+    res.sendStatus(500);
+  });
+
+  streamReq.end();
+});
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Ibiza Radio</h1>
+    <audio controls autoplay src="/ibiza"></audio>
+  `);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Radio proxy running on port ${PORT}`);
 });
